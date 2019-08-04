dd1f1db83265f724030b3f1beee731aa6ed30e14
Sandy Maguire <sandy@sandymaguire.me>
2019-08-04 17:48:45 -0400

    Test for plugin

--- cmptype-plugin/src/Type/Compare.hs
+++ cmptype-plugin/src/Type/Compare.hs
@@ -7,8 +7,11 @@ module Type.Compare where
 
 import GHC.TypeLits
 
+------------------------------------------------------------------------------
+-- | Compare two types. For 'Nat's and 'Symbol's, this uses the built-in
+-- comparisons. For all other types, "Type.Compare.Plugin" will solve it.
 type family CmpType (a :: k) (b :: k) :: Ordering where
-  CmpType a a = 'EQ
+  CmpType a             a = 'EQ
+  CmpType (a :: Nat)    b = CmpNat a b
   CmpType (a :: Symbol) b = CmpSymbol a b
-  CmpType (a :: Nat) b    = CmpNat a b
 
