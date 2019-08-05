0b8ad6e379ae6858b208f477a70117fa2bc5eb98
Sandy Maguire <sandy@sandymaguire.me>
2019-08-05 17:47:48 -0400

    line up the stars

--- magic-tyfams/ChangeLog.md
+++ magic-tyfams/ChangeLog.md
@@ -1,3 +1,7 @@
 # Changelog for type-sets
 
+## 0.1.0.0 --- 2019-08-05
+
+Initial release!
+
 ## Unreleased changes
--- magic-tyfams/test/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import ShouldTypecheck ()
-import ShouldNotTypecheck
-
-main :: IO ()
-main = testStuckInstances
-
-
