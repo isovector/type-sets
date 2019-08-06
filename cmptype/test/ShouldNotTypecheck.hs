59bbd87430e236881f24502f059b4b819bb3e5e2
Sandy Maguire <sandy@sandymaguire.me>
2019-08-06 14:08:54 -0400

    ensure the plugin does't mess with stuck cases for nats

--- cmptype/test/ShouldNotTypecheck.hs
+++ cmptype/test/ShouldNotTypecheck.hs
@@ -2,14 +2,17 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE PolyKinds      #-}
 {-# LANGUAGE TypeFamilies   #-}
+{-# LANGUAGE TypeOperators  #-}
 
 {-# OPTIONS_GHC -fdefer-type-errors
                 -fno-warn-deferred-type-errors
                 #-}
 
+-- TODO(sandy): It would be nice to have a better way of observing stuckness.
 module ShouldNotTypecheck where
 
 import Data.Proxy
+import GHC.TypeLits
 import Test.ShouldNotTypecheck
 import Type.Compare
 
@@ -22,10 +25,14 @@ testWrongType1 = id
 testWrongType2 :: Proxy (CmpType Stuck Int) -> Proxy 'GT
 testWrongType2 = id
 
+testWrongType3 :: Proxy (CmpType (0 - 1) 2) -> Proxy 'GT
+testWrongType3 = id
+
 
 testStuckInstances :: IO ()
 testStuckInstances = do
   shouldNotTypecheck testWrongType1
   shouldNotTypecheck testWrongType2
+  shouldNotTypecheck testWrongType3
 
 
